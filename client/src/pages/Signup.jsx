aecd02344efa22ae2b1eb96fe218a5a4fe774b4a
Deepesh-Chandra <140266893+Deepesh-Chandra@users.noreply.github.com>
2024-05-21 15:07:51 +0530

    🛠️: Created signup page UI

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Label, TextInput } from "flowbite-react";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="flex max-w-100vw min-h-screen mt-20 flex-col items-center md:items-start md:flex-row md:justify-center">
+      <div className=" mb-10">
+        <Link to={"/"} className="text-5xl font-bold ">
+          <span className="px-2 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
+            DC Blogs
+          </span>
+        </Link>
+        <p className=" px-2 text-sm mt-5">
+          This is my first MERN project. SignUp to get started.
+         <div>In this blog you will be able to access the admin panel free of cost.</div> 
+        </p>
+      </div>
+      <div className=" w-96">
+        <form className="flex flex-col gap-4 mx-3">
+          <div>
+            <Label value="Username" />
+            <TextInput type="text" placeholder="Username" id="username" />
+          </div>
+          <div>
+            <Label value="Email" />
+            <TextInput type="text" placeholder="Email" id="email" />
+          </div>
+          <div>
+            <Label value="Password" />
+            <TextInput type="text" placeholder="Password" id="password" />
+          </div>
+          <Button gradientDuoTone="purpleToPink" type="submit">Sign Up</Button>
+        </form>
 
-export default Signup
+        <div className="mt-2 mx-3 text-sm">
+          <span>Have an account ? </span>
+          <Link to={"/sign-in"} className="text-blue-500">Sign In</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,13 @@
 const flowbite = require("flowbite-react/tailwind");
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-    flowbite.content(),
 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [   flowbite.plugin(),],
-}
+  plugins: [
+    // ...
+    flowbite.plugin(),
+  ],
+};
