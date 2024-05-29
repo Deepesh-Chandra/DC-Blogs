311d6ea13752441a87f4bdb2bbacadf41e26dcfd
Deepesh-Chandra <140266893+Deepesh-Chandra@users.noreply.github.com>
2024-05-30 00:22:45 +0530

    Created SignIn page and fixed some bugs in SignUp page.

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,119 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, TextInput } from "flowbite-react";
 
 const Signin = () => {
-  return (
-    <div>Signin</div>
-  )
-}
+  const email = useRef();
+  const password = useRef();
 
-export default Signin
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const Navigate = useNavigate();
+
+  const onHandleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      email: email.current.value.trim(),
+      password: password.current.value.trim(),
+    };
+
+    if (
+      !formData.email ||
+      !formData.password ||
+      formData.email === "" ||
+      formData.password === ""
+    ) {
+      return setErrorMessage("All fields are required");
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/sign-in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (response.ok) {
+        Navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage("Failed to sign in. Please try again later.");
+      setLoading(false);
+    }
+  };
+  return (
+    <div
+      className="flex gap-4
+     max-w-100vw min-h-screen mt-20 flex-col items-center md:items-start md:flex-row md:justify-center"
+    >
+      <div className=" mb-10">
+        <Link to={"/"} className="text-5xl font-bold ">
+          <span className="px-2 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
+            DC Blogs
+          </span>
+        </Link>
+        <p className=" px-2 text-sm mt-5">
+          Welcone back to DC Blogs !
+        </p>
+        <div className=" px-2 text-sm">
+          In this blog you will be able to access the admin panel free of cost.
+        </div>
+      </div>
+      <div className=" w-96">
+        <form
+          onSubmit={(event) => onHandleSubmit(event)}
+          className="flex flex-col gap-4 mx-3"
+        >
+          <div>
+            <Label value="Email" />
+            <TextInput ref={email} type="email" placeholder="Email" id="email" autoComplete="email"/>
+          </div>
+          <div>
+            <Label value="Password" />
+            <TextInput
+              ref={password}
+              type="password"
+              placeholder="Password"
+              id="password"
+              autocomplete="current-password"
+            />
+          </div>
+          <Button
+            gradientDuoTone="purpleToPink"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <Spinner size="sm" light={true} />
+                <span className="pl-2">Loading...</span>
+              </>
+            ) : (
+              "Sign In"
+            )}
+          </Button>
+        </form>
+        <div className="mt-2 mx-3 text-sm">
+          <span> Don't have an account ? </span>
+          <Link to={"/sign-up"} className="text-blue-500">
+            Sign Up
+          </Link>
+        </div>
+        {errorMessage && (
+          <Alert className="mt-5" color="failure">
+            {errorMessage}
+          </Alert>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,31 +1,56 @@
-import React, { useRef } from "react";
-import { Link } from "react-router-dom";
-import { Button, Label, TextInput } from "flowbite-react";
+import React, { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 
 const Signup = () => {
-
   const username = useRef();
   const email = useRef();
   const password = useRef();
 
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const Navigate=useNavigate();
+
   const onHandleSubmit = async (event) => {
     event.preventDefault();
-    const data = {
+    const formData = {
       username: username.current.value.trim(),
       email: email.current.value.trim(),
       password: password.current.value.trim(),
     };
-    
+
+    if (
+      !formData.username ||
+      !formData.email ||
+      !formData.password ||
+      formData.username === "" ||
+      formData.email === "" ||
+      formData.password === ""
+    ) {
+      return setErrorMessage("All fields are required");
+    }
+
     try {
-      const response = await fetch("http://localhost:3000/api/auth/signup", {
+      setLoading(true);
+      setErrorMessage(null);
+      const response = await fetch("http://localhost:3000/api/auth/sign-up", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(data),
-      })
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (response.ok) {
+        Navigate("/sign-in");
+      }
     } catch (error) {
-      
+      setErrorMessage("Failed to sign up. Please try again later.");
+      setLoading(false);
     }
   };
   return (
@@ -38,31 +63,67 @@ const Signup = () => {
         </Link>
         <p className=" px-2 text-sm mt-5">
           This is my first MERN project. SignUp to get started.
-         <div>In this blog you will be able to access the admin panel free of cost.</div> 
         </p>
+        <div className=" px-2 text-sm">
+          In this blog you will be able to access the admin panel free of cost.
+        </div>
       </div>
       <div className=" w-96">
-        <form onSubmit={(event) => onHandleSubmit(event)} className="flex flex-col gap-4 mx-3">
-          
+        <form
+          onSubmit={(event) => onHandleSubmit(event)}
+          className="flex flex-col gap-4 mx-3"
+        >
           <div>
             <Label value="Username" />
-            <TextInput ref={username} type="text" placeholder="Username" id="username" />
+            <TextInput
+              ref={username}
+              type="text"
+              placeholder="Username"
+              id="username"
+              autoComplete="username"
+            />
           </div>
           <div>
             <Label value="Email" />
-            <TextInput ref={email} type="text" placeholder="Email" id="email" />
+            <TextInput ref={email} type="email" placeholder="Email" id="email" autoComplete="email"/>
           </div>
           <div>
             <Label value="Password" />
-            <TextInput ref={password} type="text" placeholder="Password" id="password" />
+            <TextInput
+              ref={password}
+              type="password"
+              placeholder="Password"
+              id="password"
+              autocomplete="current-password"
+            />
           </div>
-          <Button gradientDuoTone="purpleToPink" type="submit">Sign Up</Button>
+          <Button
+            gradientDuoTone="purpleToPink"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <Spinner size="sm" light={true} />
+                <span className="pl-2">Loading...</span>
+              </>
+            ) : (
+              "Sign Up"
+            )}
+          </Button>
         </form>
 
         <div className="mt-2 mx-3 text-sm">
           <span>Have an account ? </span>
-          <Link to={"/sign-in"} className="text-blue-500">Sign In</Link>
+          <Link to={"/sign-in"} className="text-blue-500">
+            Sign In
+          </Link>
         </div>
+        {errorMessage && (
+          <Alert className="mt-5" color="failure">
+            {errorMessage}
+          </Alert>
+        )}
       </div>
     </div>
   );
