a6fd122c0c2d6774221a0920fcafcc0bb50fdaa3
Deepesh-Chandra <140266893+Deepesh-Chandra@users.noreply.github.com>
2024-06-05 01:02:37 +0530

    🛠️ : Added Google OAuth functionality.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -44,9 +44,43 @@ export const signIn = async (req, res, next) => {
 
         const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET_KEY, { expiresIn: "1d" });
 
-        const { password:pass, ...others } = validUser._doc;
+        const { password: pass, ...others } = validUser._doc;
         res.status(200).cookie("token", token, { httpOnly: true }).json(others);
     } catch (error) {
         next(error);
     }
+}
+
+export const google = async (req, res, next) => {
+
+    const { email, name, imageUrl } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_KEY);
+
+            const { password: pass, ...others } = user._doc;
+            res.status(200).cookie("access_token", token, { httpOnly: true }).json(others);
+        } else {
+            const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+            const newUser = new User({
+                username: name.toLowerCase().split(" ").join("")+ Math.random().toString(9).slice(-4),
+                email,
+                password: hashedPassword,
+                profilePic: imageUrl
+            });
+            newUser.save();
+
+            const token = jwt.sign({ _id: newUser._id, }, process.env.JWT_SECRET_KEY);
+
+            const { password: pass, ...others } = newUser._doc;
+            res.status(200).cookie("access_token", token, { httpOnly: true }).json(others);
+
+        }
+
+    } catch (error) {
+        next(error);
+    }
+
 }
